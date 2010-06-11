afdd6acbfd1e5c503d5ca3ca2029a023a52ea09e
Simon Tuffs <simontuffs@gmail.com>
2010-06-11 07:58:13 +0000

    Update to fix problem overriding log4j.properties using one-jar.class.path

--- one-jar-log4j/src/com/simontuffs/onejar/test/log4j/Main.java
+++ one-jar-log4j/src/com/simontuffs/onejar/test/log4j/Main.java
@@ -9,12 +9,14 @@ public class Main extends Testable {
     public static Logger log = Logger.getLogger(Main.class);
     
     public static void main(String args[]) throws Exception {
+        System.out.println("classloader=" + Main.class.getClassLoader());
         log.debug("log4j.debug is enabled");
         log.info("log4j.info is enabled");
         log.warn("log4j.warn is enabled");
         log.error("log4j.error is enabled");
         Main main = new Main();
         main.runTests();
+        log.info("main is done");
     }
     
     public void testlog4j() {
