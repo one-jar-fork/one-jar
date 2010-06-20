d567fa229d3788ad23a6286caa4ac870530030bf
Simon Tuffs <simontuffs@gmail.com>
2010-06-20 19:14:45 +0000

    Update copyright information (dates mainly).

--- one-jar-appgen/src/appgen/com/simontuffs/onejar/appgen/AppGen.java
+++ one-jar-appgen/src/appgen/com/simontuffs/onejar/appgen/AppGen.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2004-2010, P. Simon Tuffs (simon@simontuffs.com)
+ * All rights reserved.
+ *
+ * See the full license at http://one-jar.sourceforge.net/one-jar-license.html
+ * This license is also included in the distributions of this software
+ * under doc/one-jar-license.txt
+ */  
 package com.simontuffs.onejar.appgen;
 
 import java.io.BufferedInputStream;
