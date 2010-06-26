433a171eaf23a6073a3879935eb32bfa4caf455e
Simon Tuffs <simontuffs@gmail.com>
2010-06-26 07:00:49 +0000

    BUG-3021667: Guice finalizer regression from 0.97 RC4.

--- one-jar-guice/src/onejar_guice/Main.java
+++ one-jar-guice/src/onejar_guice/Main.java
@@ -1,11 +1,35 @@
 package onejar_guice;
 
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.simontuffs.onejar.test.Testable;
 
 public class Main extends Testable {
 
+    protected static Throwable thrown;
+    
+    static {
+        Logger root = Logger.getLogger("");
+        root.addHandler(new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                thrown = record.getThrown();
+                System.out.println("thrown=" + thrown);
+            }
+            @Override
+            public void close() throws SecurityException {
+            }
+            @Override
+            public void flush() {
+            }
+        });
+        
+    }
+    
     public Main(){
 	}
 	
@@ -13,9 +37,13 @@ public class Main extends Testable {
 		System.out.println("Hello from Guice classloader=" + this.getClass().getClassLoader());
 	}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         Injector injector = Guice.createInjector();
         injector.getInstance(Main.class).sayHello();
+        System.out.println("thrown=" + thrown);
+        if (thrown != null) {
+            throw thrown;
+        }
     }
 
 }
--- one-jar-guice/test/com/simontuffs/onejar/guice/test/SelfTestOneJarGuice.java
+++ one-jar-guice/test/com/simontuffs/onejar/guice/test/SelfTestOneJarGuice.java
@@ -1,5 +1,10 @@
 package com.simontuffs.onejar.guice.test;
 
+import java.util.logging.Filter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
@@ -12,6 +17,26 @@ public class SelfTestOneJarGuice extends TestCase {
 
     protected static Object test;
     protected static int failures = 0;
+
+    protected static Throwable thrown;
+    
+    static {
+        Logger root = Logger.getLogger("");
+        root.addHandler(new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                thrown = record.getThrown();
+                System.out.println("thrown=" + thrown);
+            }
+            @Override
+            public void close() throws SecurityException {
+            }
+            @Override
+            public void flush() {
+            }
+        });
+        
+    }
     
     public static void main(String args[]) {
         new TestSuite(SelfTestOneJarGuice.class).run(new TestResult());
@@ -29,7 +54,12 @@ public class SelfTestOneJarGuice extends TestCase {
     }
     
     public void testHello() throws Exception {
+        System.out.println("SelfTestOneJarGuice: testHello()");
         Invoker.invoke(test, "sayHello");
+        System.out.println("thrown=" + thrown);
+        if (thrown != null) {
+            throw new RuntimeException(thrown);
+        }
     }
     
 }
